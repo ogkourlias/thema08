e43a23abe82d33f96767d305afb157557fe32c7c
denniswiersma <dennis.wiersma@icloud.com>
2022-06-18 13:55:10 +0200

    rewrote parameter definitions but everything broke

--- code/main.R
+++ code/main.R
@@ -4,64 +4,60 @@
 library(deSolve)
 library("ggplot2")
 
-# Parameterss & state will need to be defined first.
-parms_adeno <- c(
+# Parameters & state will need to be defined first.
+# Defining parameters that stay the same
+base_parameters <- c(
   # Parameters governing cell growth
   rH = 0.00275, #The per-capita growth rate of normal cells. Unit: hr^-1
   rC = 0.003, #	The per-capita growth rate of tumor cells. Unit: hr^−1
   KH = 10^11, #		The carrying capacity of normal cells. Unit: cells
-  KC = 1.47*10^12, #		The carrying capacity of tumor cells. Unit: cells
-  
-  
-  # Parameters for the Adenovirus.
+  KC = 1.47*10^12 #		The carrying capacity of tumor cells. Unit: cells
+)
+
+# Defining adeno virus specific parameters for tumour cells
+adeno_parms <- append(base_parameters, c(
   lambdaC = 1/48, # Lysing rate of tumor cells. Unit: cell^-1hr^-1
   bC = 1000, # Burst size from lysing infected tumor cells. Unit: -
   omega = 1, # Viral clearance rate. Unit: virus^-1hr^-1
   betaC = 5*10^-12 # Uptake/encounter/infection rate of tumor cells. Unit: viruses cell^−1hr^−1
-)
+))
 
-parms_adeno["bH"] = (parms_adeno["bC"] * 0.1) # Burst size of normal (Healthy) cells. Unit : -
-parms_adeno["lambdaH"] = (parms_adeno["lambdaC"] * 0.1) # Lysing rate of normal (Healthy) cells
-parms_adeno["betaH"] = (parms_adeno["betaC"] * 0.0025) #(4*10^4) # Infection rate of normal cells. Normal cell to tumor cell infection rate ratio is 0.0025
+# Appending adeno virus parameters with values for normal cells
+adeno_parms <- append(adeno_parms, c(
+  bH = (adeno_parms["bC"] * 0.1), # Burst size of normal (Healthy) cells. Unit: -
+  lamdbaH = (adeno_parms["lambdaC"] * 0.1), # Lysing rate of normal (Healthy) cells
+  betaH = (adeno_parms["betaC"] * 0.0025) #(4*10^4) # Infection rate of normal cells. Normal cell to tumor cell infection rate ratio is 0.0025
+))
 
-# Parameterss & state will need to be defined first.
-parms_hsv <- c(
-  # Parameters governing cell growth
-  rH = 0.00275, #The per-capita growth rate of normal cells. Unit: hr^-1
-  rC = 0.003, #	The per-capita growth rate of tumor cells. Unit: hr^−1
-  KH = 10^11, #		The carrying capacity of normal cells. Unit: cells
-  KC = 1.47*10^12, #		The carrying capacity of tumor cells. Unit: cells
-  
-  # Parameters for the HSV.
+# Defining hsv specific parameters for tumour cells
+hsv_parms <- append(base_parameters, c(
   lambdaC = 1/18, # Lysing rate of tumor cells. Unit: cell^-1hr^-1
   bC = 50, # Burst size from lysing infected tumor cells. Unit: -
   omega = 0.025, # Viral clearance rate. Unit: virus^-1hr^-1
   betaC = 2.5*10^-12 # Uptake/encounter/infection rate of tumor cells. Unit: viruses cell^−1hr^−1
-)
+))
 
-parms_hsv["bH"] = parms_hsv["bC"] * 0.1 # Burst size of normal (Healthy) cells. Unit : -
-parms_hsv["lambdaH"] = parms_hsv["lambdaC"] * 0.1 # Lysing rate of normal (Healthy) cells
-parms_hsv["betaH"] = parms_hsv["betaC"] * 0.0025 # Infection rate of normal cells. Normal cell to tumor cell infection rate ratio is 0.0025
+# Appending hsv parameters with values for normal cells
+hsv_parms <- append(hsv_parms, c(
+  bH = (hsv_parms["bC"] * 0.1), # Burst size of normal (Healthy) cells. Unit : -
+  lambdaH = (hsv_parms["lambdaC"] * 0.1), # Lysing rate of normal (Healthy) cells
+  betaH = (hsv_parms["betaC"] * 0.0025) # Infection rate of normal cells. Normal cell to tumor cell infection rate ratio is 0.0025
+))
 
-
-# Parameterss & state will need to be defined first.
-parms_vsv <- c(
-  # Parameters governing cell growth
-  rH = 0.00275, #The per-capita growth rate of normal cells. Unit: hr^-1
-  rC = 0.003, #	The per-capita growth rate of tumor cells. Unit: hr^−1
-  KH = 10^11, #		The carrying capacity of normal cells. Unit: cells
-  KC = 1.47*10^12, #		The carrying capacity of tumor cells. Unit: cells
-  
-  # Parameters for VSV.
+# Defining vsv specific parameters for tumour cells
+vsv_parms <- append(base_parameters, c(
   lambdaC = 1/24, # Lysing rate of tumor cells. Unit: cell^-1hr^-1
   bC = 1350, # Burst size from lysing infected tumor cells. Unit: -
   omega = 0.244, # Viral clearance rate. Unit: virus^-1hr^-1
   betaC = 5*10^-13 # Uptake/encounter/infection rate of tumor cells. Unit: viruses cell^−1hr^−1
-)
+))
 
-parms_vsv["bH"] = parms_vsv["bC"] * 0.1 # Burst size of normal (Healthy) cells. Unit : -
-parms_vsv["lambdaH"] = parms_vsv["lambdaC"] * 0.1 # Lysing rate of normal (Healthy) cells
-parms_vsv["betaH"] = parms_vsv["betaC"] * 0.0025 # Infection rate of normal cells. Normal cell to tumor cell infection rate ratio is 0.0025
+# Appending vsv parameters with values for normal cells
+vsv_parms <- append(vsv_parms, c(
+  bH = (vsv_parms["bC"] * 0.1), # Burst size of normal (Healthy) cells. Unit : -
+  lambdaH = (vsv_parms["lambdaC"] * 0.1), # Lysing rate of normal (Healthy) cells
+  betaH = (vsv_parms["betaC"] * 0.0025) # Infection rate of normal cells. Normal cell to tumor cell infection rate ratio is 0.0025
+))
 
 # Defining the states
 state <- c(
@@ -100,9 +96,9 @@ vir <- function(t, state, parameters){
 }
 
 # Defining the desolve outputs to their respective variables.
-adeno_res<- ode(y = state, times = times, func = vir, parms = parms_adeno)
-hsv_res <- ode(y = state, times = times, func = vir, parms = parms_hsv)
-vsv_res <- ode(y = state, times = times, func = vir, parms = parms_vsv)
+adeno_res<- ode(y = state, times = times, func = vir, parms = adeno_parms)
+hsv_res <- ode(y = state, times = times, func = vir, parms = hsv_parms)
+vsv_res <- ode(y = state, times = times, func = vir, parms = vsv_parms)
 
 # Applying post processing Log10 transformations for all values but time.
 adeno_res[,2:6] <- log10(adeno_res[,2:6] + 1)
@@ -198,17 +194,17 @@ plot(hsv_res)
 plot(vsv_res)
 
 # Increasing the burst and lysing rates of normal cells
-parms_adeno["bH"] = (parms_adeno["bC"] * 0.3) # Burst size of normal (Healthy) cells. Unit : -
-parms_adeno["lambdaH"] = (parms_adeno["lambdaC"] * 0.3) # Lysing rate of normal (Healthy) cells
-parms_hsv["bH"] = (parms_hsv["bC"] * 0.3) # Burst size of normal (Healthy) cells. Unit : -
-parms_hsv["lambdaH"] = (parms_hsv["lambdaC"] * 0.3) # Lysing rate of normal (Healthy) cells
-parms_vsv["bH"] = (parms_vsv["bC"] * 0.3) # Burst size of normal (Healthy) cells. Unit : -
-parms_vsv["lambdaH"] = (parms_vsv["lambdaC"] * 0.3) # Lysing rate of normal (Healthy) cells
+adeno_parms["bH"] = (adeno_parms["bC"] * 0.3) # Burst size of normal (Healthy) cells. Unit : -
+adeno_parms["lambdaH"] = (adeno_parms["lambdaC"] * 0.3) # Lysing rate of normal (Healthy) cells
+hsv_parms["bH"] = (hsv_parms["bC"] * 0.3) # Burst size of normal (Healthy) cells. Unit : -
+hsv_parms["lambdaH"] = (hsv_parms["lambdaC"] * 0.3) # Lysing rate of normal (Healthy) cells
+vsv_parms["bH"] = (vsv_parms["bC"] * 0.3) # Burst size of normal (Healthy) cells. Unit : -
+vsv_parms["lambdaH"] = (vsv_parms["lambdaC"] * 0.3) # Lysing rate of normal (Healthy) cells
 
 # Defining the desolve outputs to their respective variables.
-adeno_res_2<- ode(y = state, times = times, func = vir, parms = parms_adeno)
-hsv_res_2 <- ode(y = state, times = times, func = vir, parms = parms_hsv)
-vsv_res_2 <- ode(y = state, times = times, func = vir, parms = parms_vsv)
+adeno_res_2<- ode(y = state, times = times, func = vir, parms = adeno_parms)
+hsv_res_2 <- ode(y = state, times = times, func = vir, parms = hsv_parms)
+vsv_res_2 <- ode(y = state, times = times, func = vir, parms = vsv_parms)
 
 # Applying post processing Log10 transformations for all values but time.
 adeno_res_2[,2:6] <- log10(adeno_res_2[,2:6] + 1)
@@ -221,20 +217,20 @@ plot(hsv_res_2)
 plot(vsv_res_2)
 
 # Increasing the burst and lysing rates of normal cells
-parms_adeno["bH"] = (parms_adeno["bC"] * 0.1) # Burst size of normal (Healthy) cells. Unit : -
-parms_adeno["lambdaH"] = (parms_adeno["lambdaC"] * 0.1) # Lysing rate of normal (Healthy) cells
-parms_hsv["bH"] = (parms_hsv["bC"] * 0.1) # Burst size of normal (Healthy) cells. Unit : -
-parms_hsv["lambdaH"] = (parms_hsv["lambdaC"] * 0.1) # Lysing rate of normal (Healthy) cells
-parms_vsv["bH"] = (parms_vsv["bC"] * 0.1) # Burst size of normal (Healthy) cells. Unit : -
-parms_vsv["lambdaH"] = (parms_vsv["lambdaC"] * 0.1) # Lysing rate of normal (Healthy) cells
-parms_adeno["betaH"] = 0 # Infection rate of normal cells. Normal cell to tumor cell infection rate ratio is 0.0025
-parms_hsv["betaH"] = 0 # Infection rate of normal cells. Normal cell to tumor cell infection rate ratio is 0.0025
-parms_vsv["betaH"] = 0 # Infection rate of normal cells. Normal cell to tumor cell infection rate ratio is 0.0025
+adeno_parms["bH"] = (adeno_parms["bC"] * 0.1) # Burst size of normal (Healthy) cells. Unit : -
+adeno_parms["lambdaH"] = (adeno_parms["lambdaC"] * 0.1) # Lysing rate of normal (Healthy) cells
+hsv_parms["bH"] = (hsv_parms["bC"] * 0.1) # Burst size of normal (Healthy) cells. Unit : -
+hsv_parms["lambdaH"] = (hsv_parms["lambdaC"] * 0.1) # Lysing rate of normal (Healthy) cells
+vsv_parms["bH"] = (vsv_parms["bC"] * 0.1) # Burst size of normal (Healthy) cells. Unit : -
+vsv_parms["lambdaH"] = (vsv_parms["lambdaC"] * 0.1) # Lysing rate of normal (Healthy) cells
+adeno_parms["betaH"] = 0 # Infection rate of normal cells. Normal cell to tumor cell infection rate ratio is 0.0025
+hsv_parms["betaH"] = 0 # Infection rate of normal cells. Normal cell to tumor cell infection rate ratio is 0.0025
+vsv_parms["betaH"] = 0 # Infection rate of normal cells. Normal cell to tumor cell infection rate ratio is 0.0025
 
 # Defining the desolve outputs to their respective variables.
-adeno_res_3<- ode(y = state, times = times, func = vir, parms = parms_adeno)
-hsv_res_3 <- ode(y = state, times = times, func = vir, parms = parms_hsv)
-vsv_res_3 <- ode(y = state, times = times, func = vir, parms = parms_vsv)
+adeno_res_3<- ode(y = state, times = times, func = vir, parms = adeno_parms)
+hsv_res_3 <- ode(y = state, times = times, func = vir, parms = hsv_parms)
+vsv_res_3 <- ode(y = state, times = times, func = vir, parms = vsv_parms)
 
 # Applying post processing Log10 transformations for all values but time.
 adeno_res_3[,2:6] <- log10(adeno_res_3[,2:6] + 1)
